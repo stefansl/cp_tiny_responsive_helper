d4047f86a471619a7d94368523ce11e12fa4c6f7
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2014-01-28 15:17:17 +0100

    Initial Commit

---
+++ config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Cp_tiny_responsive_helper
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'moo_cp_tiny_responsive_helper' => 'system/modules/cp_tiny_responsive_helper/templates',
+	'j_cp_tiny_responsive_helper'   => 'system/modules/cp_tiny_responsive_helper/templates',
+));
